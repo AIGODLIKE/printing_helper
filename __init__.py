73bd199f26a2fc7f0e0fcaaf5284ab30bed9cedf
小萌新 working <3209970865@qq.com>
2025-10-31 09:40:28 +0800

    Feat: init file

---
+++ __init__.py
@@ -0,0 +1,22 @@
+from . import register_module
+
+bl_info = {
+    "name": "Printing Helper",
+    "description": "Printing Helper",
+    "author": "小萌新",
+    "version": (0, 1, 0),
+    "blender": (4, 2, 0),
+    "location": "",
+    "support": "COMMUNITY",
+    "category": "3D View",
+}
+
+ADDON_VERSION = ".".join([str(v) for v in bl_info["version"]])
+
+
+def register():
+    register_module.register()
+
+
+def unregister():
+    register_module.unregister()
